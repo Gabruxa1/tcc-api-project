0c0afb6853bba8989e025d4b4cde89a3934f3778
Gabriel Cordeiro <gabriel.souzatb@hotmail.com>
2024-06-10 09:58:38 -0300

    update de metodo para resgatar relatório

--- src/controllers/relatorioController.js
+++ src/controllers/relatorioController.js
@@ -5,9 +5,14 @@ const getReport = async (request, response) => {
 		const { id } = request.params;
 		const { data_inicio, data_fim } = request.query;
 		const report = await relatorioModel.getReport(id, data_inicio, data_fim);
+
 		return response.status(200).json(report);
 	} catch (error) {
-		return response.status(500).json({ error: "Erro ao obter dados do relatório.", details: error.message });
+		if (error.message.includes("Não existem pontos registrados para o período selecionado")) {
+			return response.status(400).json({ error: error.message });
+		} else {
+			return response.status(500).json({ error: "Erro ao obter dados do relatório.", details: error.message });
+		}
 	}
 };
 
