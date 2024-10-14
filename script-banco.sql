9e08335a1017d2d1f9684025d924739ca28214dd
Gabriel Cordeiro <gabriel.harik@gmail.com>
2024-10-14 02:57:30 -0300

    update

--- script-banco.sql
+++ script-banco.sql
@@ -1,65 +1,13 @@
---script da criação das tabelas
-CREATE TABLE IF NOT EXISTS pessoas (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    cpf VARCHAR(11) NOT NULL,
-    telefone VARCHAR(11) NOT NULL
-);
+select * from funcionarios;
 
-CREATE TABLE IF NOT EXISTS funcionarios (
-    pessoa_id INTEGER PRIMARY KEY REFERENCES pessoas(id) ON DELETE CASCADE,
-    email VARCHAR(255) NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    funcao VARCHAR(100) NOT NULL,
-    admin BOOLEAN NOT NULL,
-    ativo BOOLEAN NOT NULL,
-    custo_hora FLOAT NOT NULL
-);
+select * from pessoas;
 
-CREATE TABLE IF NOT EXISTS registros_pontos (
-    pessoa_id INTEGER REFERENCES pessoas(id) ON DELETE CASCADE,
-    data DATE,
-    entrada TIME,
-    saida TIME
-);
-
---funcao para inserção em ambas as tabelas
-CREATE OR REPLACE FUNCTION inserir_pessoa_funcionario(
-    nome_p VARCHAR(255),
-    cpf_p VARCHAR(11),
-    telefone_p VARCHAR(11),
-    email_f VARCHAR(255),
-    senha_f VARCHAR(255),
-    funcao_f VARCHAR(100),
-    admin_f BOOLEAN,
-    ativo_f BOOLEAN,
-    custo_hora_f FLOAT
-)
-RETURNS INTEGER AS $$
-DECLARE
-    id_pessoa INTEGER;
-BEGIN
-    INSERT INTO pessoas (nome, cpf, telefone) VALUES (nome_p, cpf_p, telefone_p) RETURNING id INTO id_pessoa;
-    
-    INSERT INTO funcionarios (pessoa_id, email, senha, funcao, admin, ativo, custo_hora) 
-        VALUES (id_pessoa, email_f, senha_f, funcao_f, admin_f, ativo_f, custo_hora_f);
-    
-    RETURN id_pessoa; -- Retorna o id_pessoa gerado
-END;
-$$ LANGUAGE PLPGSQL;
-
---script de deleção se necessário
-drop table registros_pontos;
-drop table funcionarios;
-drop table pessoas;
-
-select*from pessoas rp;
-select*from funcionarios f;
-select*from registros_pontos rp;
+select*from registros_pontos rp
+where pessoa_id =14;
 
 -- Inserir 10 pessoas distintas na tabela pessoas com nomes fictícios
-insert into pessoas (nome, cpf, telefone)
-values
+INSERT INTO pessoas (nome, cpf, telefone)
+VALUES
     ('João Silva', '11111111111', '12345678901'),
     ('Maria Oliveira', '22222222222', '23456789012'),
     ('José Santos', '33333333333', '34567890123'),
@@ -72,8 +20,8 @@ values
     ('Isabela Rodrigues', '10101010101', '01234567890');
    
    
-insert into funcionarios (pessoa_id, email, senha, funcao, admin, ativo, custo_hora)
-values
+INSERT INTO funcionarios (pessoa_id, email, senha, funcao, admin, ativo, custo_hora)
+VALUES
     (1, 'email1@example.com', '$2b$10$6nAegm525fqTsrd/CqEeiukHypD8LJAR2RlzXspVfLONtklePxMiO', 'Gerente', true, true, 20.0), --user admin, senha "teste123"
     (2, 'email2@example.com', '$2b$10$6nAegm525fqTsrd/CqEeiukHypD8LJAR2RlzXspVfLONtklePxMiO', 'Analista', false, true, 25.0), --user normal, senha "teste123"
     (3, 'email3@example.com', '$2b$10$6nAegm525fqTsrd/CqEeiukHypD8LJAR2RlzXspVfLONtklePxMiO', 'Desenvolvedor', true, false, 22.0),
