5cf03c337278dddfe2a288027a234a63d7b2c9c2
Gabriel Cordeiro <gabriel.harik@gmail.com>
2024-10-14 22:05:55 -0300

    correcao logica de expiracao token

--- models/authModel.js
+++ models/authModel.js
@@ -6,6 +6,7 @@ const authenticate = async (email) => {
 	// Busca o usuário no banco de dados pelo email fornecido
 	const user = await funcionariosModel.getUserByEmail(email);
 
+
 	// Verifica se o usuário foi encontrado
 	if (!user) {
 		return null; // Retorna null se o usuário não for encontrado
@@ -14,7 +15,7 @@ const authenticate = async (email) => {
 	if (user) {
 		// Retorna os dados essenciais do usuário, como ID, email, senha, status de administrador e ativo/inativo
 		return {
-			id: user.id,
+			id: user.pessoa_id,
 			email: user.email,
 			senha: user.senha,
 			admin: user.admin,
@@ -28,14 +29,13 @@ const generateAuthToken = (user) => {
 	// Cria o token JWT usando o método sign, que inclui dados relevantes do usuário
 	const token = jwt.sign(
 		{
-			id: user.id,        // ID do usuário
+			id: user.id,// ID do usuário
 			email: user.email,  // Email do usuário
 			admin: user.admin,  // Status de administrador (true/false)
 		},
 		process.env.JWT_SECRET, // Chave secreta usada para criptografar o token
-		{ expiresIn: process.env.JWT_EXPIRES_IN || "1d" } // Tempo de expiração, podendo ser configurado via .env
+		{ expiresIn: process.env.JWT_EXPIRES_IN } // Tempo de expiração, podendo ser configurado via .env
 	);
-
 	// Retorna o token gerado
 	return token;
 };
